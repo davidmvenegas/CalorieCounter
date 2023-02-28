09f9df4e3ea75b4dd5faf82f49229311cdfa3b45
David Venegas <venegasdavidm@gmail.com>
2023-02-28 08:49:39 -0500

    Initial Commit

---
+++ calorie-counter/calorie_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorie_counterApp.swift
+//  calorie-counter
+//
+//  Created by David Venegas on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct calorie_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
